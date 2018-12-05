d9f4403d28e7d23623a066f0a03f4f882c248cf5
Phi Tran <phitran@Phis-MBP.attlocal.net>
2018-12-05 14:58:51 -0600

    styling, type.js, icon changes

---
+++ JS/js.js
@@ -0,0 +1,11 @@
+// Type.js //
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    loop: true,
+    // loopCount: null,
+    showCursor: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+
+  });
