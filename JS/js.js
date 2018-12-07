8288f2a4accee422cdf77401fd4eb18a608b9291
Phi Tran <phitran@Phis-MacBook-Pro.local>
2018-12-06 19:22:53 -0600

    test change

--- JS/js.js
+++ JS/js.js
@@ -1,5 +1,4 @@
 // Type.js //
-
 var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
     loop: true,
@@ -7,5 +6,26 @@ var typed = new Typed('#typed', {
     showCursor: true,
     typeSpeed: 100,
     backSpeed: 100,
-
   });
+
+//  media picture //
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+       
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
