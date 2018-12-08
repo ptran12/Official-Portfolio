3e1914128e14e25e8138f0e5eb26deca9947b81f
Phi Tran <phitran@Phis-MBP.attlocal.net>
2018-12-08 17:06:38 -0600

    fixed background image

--- JS/js.js
+++ JS/js.js
@@ -18,14 +18,3 @@ document.addEventListener('DOMContentLoaded', function() {
     $('.materialboxed').materialbox();
   });
        
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.parallax').parallax();
-  });
-        
